fc0437b96f2f96461861fe53280527f40d0f648b
AkshayKPrajapati <akshayprajapati6200@outlook.com>
2026-04-02 13:20:55 +0530

    working on delete class

--- database/jdbc/operation.sql
+++ database/jdbc/operation.sql
@@ -13,8 +13,13 @@ insert into employee(id, name, salary) values(114, 'Akshay', 5000000.00);
 insert into employee(id, name, salary) values(115, 'Rahul', 6000000.00);
 insert into employee(id, name, salary) values(116, 'Raj', 7000000.00);
 insert into employee(id, name, salary) values(117, 'Ravi', 8000000.00);
-
+insert into employee(id, name, salary) values(1001, 'Neha', 2500.00);
 
 select * from employee;
 truncate table employee;
 
+select * from employee where id=1001;
+update employee set salary=salary+100000 where id=1001;
+
+delete from employee where id=1001;
+DELETE FROM `016_25batch`.`employee` WHERE (`id` = '1001');
