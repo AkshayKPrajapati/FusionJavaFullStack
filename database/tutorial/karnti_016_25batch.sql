4738cebf110b165e0da6b4f6926b7df8e9c9cb23
AkshayKPrajapati <akshayprajapati6200@outlook.com>
2026-05-10 22:02:19 +0530

    table student created

--- database/tutorial/karnti_016_25batch.sql
+++ database/tutorial/karnti_016_25batch.sql
@@ -1,3 +1,14 @@
 -- create database
 create database karnti_016_25batch; 
 USE karnti_016_25batch;
+
+-- create student table
+create table student (
+    s_id int primary key,
+    s_name varchar(20) not null,
+    contact bigint,
+    location varchar(255) default 'Pune',
+    dob date,
+    gender enum('Male','Female','Other'),
+    age int check(age>=18)
+);
